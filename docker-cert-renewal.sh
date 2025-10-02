de0aa055ca3f8aaaa672e2b3a19889a972a81cdf
Cristian Uroz <cristian@uroz.cat>
2025-10-02 10:43:05 +0800

    Certbot: Reload nginx only if cert is renewed

--- docker-cert-renewal.sh
+++ docker-cert-renewal.sh
@@ -2,14 +2,13 @@
 
 # Run Certbot to renew certificates
 echo "Attempting to renew certificates..."
-docker compose run --rm certbot renew
+docker compose run --rm certbot renew --deploy-hook "/usr/local/bin/docker-reload-nginx.sh"
 
 # Check if renewal was successful
 if [ $? -ne 0 ]; then
     echo "Certbot renewal failed."
-else
-    echo "Certbot renewal successful. Reloading Nginx..."
-    docker compose kill -s HUP nginx
-    echo "Nginx reloaded."
+    exit 1
 fi
 
+echo "Certbot finished. If certificates were renewed, Nginx was reloaded."
+exit 0
